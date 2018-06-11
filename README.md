057c126f701dd94bdc398e74601e5e5bda20201b
Felix Fontein <felix@fontein.de>
2018-06-11 07:11:28 +0200

    Added BIND9. Added DNS challenge test (with wildcard cert).

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A [Docker](https://en.wikipedia.org/wiki/Docker_(software))-based test suite for Ansible's [acme_account](https://docs.ansible.com/ansible/devel/modules/acme_account_module.html), [acme_certificate](https://docs.ansible.com/ansible/devel/modules/acme_certificate_module.html) and [acme_certificate_revoke](https://docs.ansible.com/ansible/devel/modules/acme_certificate_revoke_module.html) modules.
 
-Uses [Pebble](https://github.com/letsencrypt/Pebble) as the ACME-based CA and [nginx](https://nginx.org/) as a webserver (for fulfilling `http-01` challenges).
+Uses [Pebble](https://github.com/letsencrypt/Pebble) as the ACME-based CA, [nginx](https://nginx.org/) as a webserver (for fulfilling `http-01` challenges) and [BIND 9](https://www.isc.org/downloads/bind/) as a DNS resolver (for fulfilling `dns-01` challenges).
 
 ## Usage
 
@@ -12,8 +12,9 @@ Run `./run.sh` to run the tests with these docker containers.
 
 ## TODO
 
-- Add DNS challenge support
-  * run DNS service in container
-  * use this DNS service as resolver for container ([Pebble's resolver cannot be configured](https://github.com/letsencrypt/pebble/issues/33#issuecomment-314484981))
 - Add more tests
+  - certificates for multiple domains
+  - revocation of certificates
+  - changing account data
+- Add test verifications (for account deactivation etc.)
 - With a later version of Pebble, add test for account key change
--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ build_pebble() {
     trap "{ rm -rf build ; }" EXIT
 
     mkdir build
-    cp nginx-pebble pebble-config.json build/
+    cp nginx-pebble pebble-config.json bind.conf example.org example.com build/
     # Start script
     echo "#!/bin/sh" > "build/run.sh"
+    echo "echo nameserver 127.0.0.1 > /etc/resolv.conf" >> "build/run.sh"
+    echo "service bind9 start" >> "build/run.sh"
     echo "service nginx start" >> "build/run.sh"
     echo "cd /root/go/src/github.com/letsencrypt/pebble" >> "build/run.sh"
     echo "/root/go/bin/pebble -config /root/go/src/github.com/letsencrypt/pebble/test/config/pebble-config.json" >> "build/run.sh"
@@ -16,11 +18,16 @@ build_pebble() {
     echo "FROM golang:1.10-stretch" > build/Dockerfile
     # Install software
     echo "RUN apt-get update" >> build/Dockerfile
-    echo "RUN apt-get install -y nginx $1" >> build/Dockerfile
+    echo "RUN apt-get install -y nginx bind9 $1" >> build/Dockerfile
     # Setup nginx
     echo "ADD nginx-pebble /etc/nginx/sites-available/pebble" >> build/Dockerfile
     echo "RUN mkdir /lechallenges && rm /etc/nginx/sites-enabled/default && ln -s /etc/nginx/sites-available/pebble /etc/nginx/sites-enabled/pebble" >> build/Dockerfile
-    echo "RUN service nginx start" >> build/Dockerfile
+    echo "RUN service nginx restart" >> build/Dockerfile
+    # Setup bind9
+    echo "ADD bind.conf /etc/bind/named.conf" >> build/Dockerfile
+    echo "RUN mkdir /etc/bind/zones" >> build/Dockerfile
+    echo "ADD example.com example.org /etc/bind/zones/" >> build/Dockerfile
+    echo "RUN service bind9 restart" >> build/Dockerfile
     # Install pebble
     echo "ENV GOPATH=/root/go" >> build/Dockerfile
     echo "RUN go get -u github.com/letsencrypt/pebble/..." >> build/Dockerfile
