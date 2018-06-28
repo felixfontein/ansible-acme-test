cc04153c3742fee9287db2d37120aa0e656c3339
Felix Fontein <felix@fontein.de>
2018-06-28 14:50:28 +0200

    Using new version of Pebble. Limiting sleep time to max 5 seconds.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull golang:1.10-stretch
-PEBBLE_CHECKOUT=703daa840c5306f55085548ad8fb26b5adbd9daf
+PEBBLE_CHECKOUT=25448686e9b499e42380ddf965d8e23bd794378c
 docker image build --build-arg PEBBLE_CHECKOUT=${PEBBLE_CHECKOUT} -t felixfontein/acme-test-container:${PEBBLE_CHECKOUT} docker-container
 docker push felixfontein/acme-test-container:${PEBBLE_CHECKOUT}
