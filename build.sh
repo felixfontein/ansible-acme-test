7cff41b840ba46ae19a680cabb05a408a5584fd6
Felix Fontein <felix@fontein.de>
2018-07-21 09:36:49 +0200

    Improving container build.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker pull golang:1.10-stretch
+docker pull debian:stretch-slim
 PEBBLE_CHECKOUT=25448686e9b499e42380ddf965d8e23bd794378c
 docker image build --build-arg PEBBLE_CHECKOUT=${PEBBLE_CHECKOUT} -t felixfontein/acme-test-container:${PEBBLE_CHECKOUT} docker-container
 docker push felixfontein/acme-test-container:${PEBBLE_CHECKOUT}
