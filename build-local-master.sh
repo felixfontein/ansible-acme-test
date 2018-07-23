f957a2dd5efd5991e9627a0edea23f1fa9c0511c
Felix Fontein <felix@fontein.de>
2018-07-23 08:26:25 +0200

    Forgot to update build scripts.

--- build-local-master.sh
+++ build-local-master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker pull golang:1.10-stretch
-# docker pull debian:stretch-slim
+# docker pull python:3.6-slim-stretch
 docker image build --build-arg PEBBLE_CHECKOUT=master -t local/ansible/acme-test-container:latest docker-container
