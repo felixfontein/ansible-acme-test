fa9bd6f73fb3670ac9ab2a70ae503312baf064b6
Felix Fontein <felix@fontein.de>
2018-08-20 21:42:36 +0200

    Bump Pebble version. Enable Pebble strict mode. Enable account key rollover tests.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull golang:1.10-stretch
 docker pull python:3.6-slim-stretch
-PEBBLE_CHECKOUT=2207fb14d92b9133bb5b3c7dc48ff70a474d4342
+PEBBLE_CHECKOUT=501c9cb79684dde81d2ad88039ccaa2a2f5ea7c2
 docker image build --build-arg PEBBLE_CHECKOUT=${PEBBLE_CHECKOUT} -t felixfontein/acme-test-container:${PEBBLE_CHECKOUT} docker-container
 docker push felixfontein/acme-test-container:${PEBBLE_CHECKOUT}
