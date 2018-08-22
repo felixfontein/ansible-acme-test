93fdb94952d934b0d2514d2ae777c27a6b76ef07
Felix Fontein <felix@fontein.de>
2018-08-22 16:09:32 +0200

    Bumping Pebble version. Adjusting to draft-05 of TLS-ALPN-01.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull golang:1.10-stretch
 docker pull python:3.6-slim-stretch
-PEBBLE_CHECKOUT=501c9cb79684dde81d2ad88039ccaa2a2f5ea7c2
+PEBBLE_CHECKOUT=35f569333bca2b780147889c42d9f75fa8770057
 docker image build --build-arg PEBBLE_CHECKOUT=${PEBBLE_CHECKOUT} -t felixfontein/acme-test-container:${PEBBLE_CHECKOUT} docker-container
 docker push felixfontein/acme-test-container:${PEBBLE_CHECKOUT}
