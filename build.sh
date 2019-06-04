e5769c45caf3b5be600705ca2c61bc9687b91bd2
Felix Fontein <felix@fontein.de>
2019-06-04 06:21:44 +0200

    Bump Peble version.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull golang:1.10-stretch
 docker pull python:3.6-slim-stretch
-PEBBLE_CHECKOUT=bc4da68d49d78a38bfea869d38faadc8c714654c
+PEBBLE_CHECKOUT=c5de6e0ed64c8854fd27f85067ccbbb2dbe77862
 docker image build --network host --build-arg PEBBLE_CHECKOUT=${PEBBLE_CHECKOUT} -t felixfontein/acme-test-container:${PEBBLE_CHECKOUT} docker-container
 docker push felixfontein/acme-test-container:${PEBBLE_CHECKOUT}
